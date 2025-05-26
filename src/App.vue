03e8baad226b87ef7e0409cd1d9d539e87f90264
ari wibowo <ariwibowo.edu@gmail.com>
2025-05-27 00:06:48 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+    
+<script setup>
+import NavbarComp from '@/components/partials/navigations/NavbarComp.vue'
+import FooterComp from '@/components/partials/FooterComp.vue'
+</script>
+
+
+<template>
+  <navbar-comp />
+  <div class="mx-auto max-w-7xl items-center px-4 lg:px-4">
+    <router-view />
+  </div>
+  <footer-comp />
+</template>
---
+++ src/components/includes/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDR6fDpoz9dDBNWhq2-qD-VVem0iqUK4Lo",
+    authDomain: "music-3eefa.firebaseapp.com",
+    projectId: "music-3eefa",
+    storageBucket: "music-3eefa.firebasestorage.app",
+    messagingSenderId: "141235836647",
+    appId: "1:141235836647:web:e847e01b051ef90b43b442"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
+
+export { db }
